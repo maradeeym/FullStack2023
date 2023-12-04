085927ba881b8fa1463ef6b2f7c13979645b8c35
Martin <martin.kalliola@gmail.com>
2023-12-04 23:42:29 +0200

    maiden_tiedot DONE

--- osa2/maiden_tiedot/src/App.jsx
+++ osa2/maiden_tiedot/src/App.jsx
@@ -5,6 +5,7 @@ const App = () => {
   const [allCountries, setAllCountries] = useState([]);
   const [visibleCountries, setVisibleCountries] = useState([]);
   const [filter, setFilter] = useState('');
+  const [weather, setWeather] = useState(null);
 
   useEffect(() => {
     const baseUrl = 'https://studies.cs.helsinki.fi/restcountries/api/all/';
@@ -23,6 +24,8 @@ const App = () => {
     filterCountries(event.target.value);
   };
 
+
+
   const filterCountries = (input) => {
     if (input) {
       const filteredCountries = allCountries.filter(country =>
@@ -40,19 +43,25 @@ const App = () => {
     filterCountries(countryName);
   };
 
-  const renderCountryDetails = (country) => {
-    const languages = Object.values(country.languages).join(', ');
+  useEffect(() => {
+    if (visibleCountries.length === 1) {
+      const capital = visibleCountries[0].capital[0];
+      const api_key = "be507c2cedaeed3b95121b25d34892f4"
+      //const api_key = import.meta.env.VITE_SOME_KEY
+      const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=${capital}&appid=${api_key}&units=metric`;
 
-    return (
-      <div>
-        <h2>{country.name.common}</h2>
-        <p><strong>Capital:</strong> {country.capital[0]}</p>
-        <p><strong>Population:</strong> {country.population.toLocaleString()}</p>
-        <p><strong>Languages:</strong> {languages}</p>
-        <img src={country.flags.png} alt={`Flag of ${country.name.common}`} style={{ width: '150px' }} />
-      </div>
-    );
-  };
+      axios.get(weatherUrl)
+        .then(response => {
+          setWeather(response.data);
+        })
+        .catch(error => {
+          console.error('Error fetching weather:', error);
+          setWeather(null); // Reset weather data on error
+        });
+    } else {
+      setWeather(null); // Reset weather data when more than one country is visible
+    }
+  }, [visibleCountries]);
 
   return (
     <div>
@@ -62,21 +71,37 @@ const App = () => {
         onChange={handleFilterChange}
         placeholder='Enter country name'
       />
-      {visibleCountries.length === 1
-        ? renderCountryDetails(visibleCountries[0])
-        : (
-          <ul>
-            {visibleCountries.map((country, index) => (
-              <li key={country.name.common + index}>
-                {country.name.common}
-                <button onClick={() => handleShowCountry(country.name.common)}>Show</button>
-              </li>
-            ))}
-          </ul>
-        )
-      }
+  
+      {visibleCountries.length === 1 ? (
+        <>
+          <div>
+            <h2>{visibleCountries[0].name.common}</h2>
+            <p><strong>Capital:</strong> {visibleCountries[0].capital[0]}</p>
+            <p><strong>Population:</strong> {visibleCountries[0].population.toLocaleString()}</p>
+            <p><strong>Languages:</strong> {Object.values(visibleCountries[0].languages).join(', ')}</p>
+            <img src={visibleCountries[0].flags.png} alt={`Flag of ${visibleCountries[0].name.common}`} style={{ width: '150px' }} />
+          </div>
+          {weather && (
+            <div>
+              <h3>Weather in {weather.name}</h3>
+              <p><strong>Temperature:</strong> {weather.main.temp}°C</p>
+              <p><strong>Condition:</strong> {weather.weather[0].main}</p>
+              <img src={`http://openweathermap.org/img/w/${weather.weather[0].icon}.png`} alt="Weather icon" />
+            </div>
+          )}
+        </>
+      ) : (
+        <ul>
+          {visibleCountries.map((country, index) => (
+            <li key={country.name.common + index}>
+              {country.name.common}
+              <button onClick={() => handleShowCountry(country.name.common)}>Show</button>
+            </li>
+          ))}
+        </ul>
+      )}
     </div>
-  );
+  );  
 };
 
 export default App;
