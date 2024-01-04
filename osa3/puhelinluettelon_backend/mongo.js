dabde79b36504935dd3b0c6d478040231191aa9d
Martin <martin.kalliola@gmail.com>
2024-01-04 15:57:20 +0200

    3.22

--- osa3/puhelinluettelon_backend/mongo.js
+++ osa3/puhelinluettelon_backend/mongo.js
@@ -1,13 +1,13 @@
 const mongoose = require('mongoose')
 
 if (process.argv.length < 3) {
-    console.log('Usage: node mongo.js <password> <name> <number>');
-    process.exit(1);
-  }
+  console.log('Usage: node mongo.js <password> <name> <number>')
+  process.exit(1)
+}
 
-const password = process.argv[2];
-const nameArg = process.argv[3];
-const numberArg = process.argv[4];
+const password = process.argv[2]
+const nameArg = process.argv[3]
+const numberArg = process.argv[4]
 
 const url =
   `mongodb+srv://serkkupoika:${password}@cluster0.sgksjwl.mongodb.net/puhelinluettelo?retryWrites=true&w=majority`
@@ -16,48 +16,47 @@ mongoose.set('strictQuery', false)
 mongoose.connect(url)
 
 const personSchema = new mongoose.Schema({
-    name: String,
-    number: {
-      type: String,
-      validate: {
-        validator: function(v) {
-          return /\d{3}-\d{7}/.test(v);
-        },
-        message: props => `${props.value} is not a valid phone number!`
-      }
+  name: String,
+  number: {
+    type: String,
+    validate: {
+      validator: function (v) {
+        return /\d{3}-\d{7}/.test(v)
+      },
+      message: props => `${props.value} is not a valid phone number!`
     }
-  });
-  
+  }
+})
 
 const Person = mongoose.model('Person', personSchema)
 
 if (process.argv.length === 3) {
-    // Print the database
-    Person.find({})
-        .then(persons => {
-            console.log("phonebook:");
-            persons.forEach(person => {
-                console.log(person.name + " " + person.number);
-            });
-            mongoose.connection.close();
-        });
+  // Print the database
+  Person.find({})
+    .then(persons => {
+      console.log('phonebook:')
+      persons.forEach(person => {
+        console.log(person.name + ' ' + person.number)
+      })
+      mongoose.connection.close()
+    })
 } else if (process.argv.length === 5) {
-    // Add a new person
-    const nameArg = process.argv[3];
-    const numberArg = process.argv[4];
+  // Add a new person
+  const nameArg = process.argv[3]
+  const numberArg = process.argv[4]
 
-    const person = new Person({
-        name: nameArg,
-        number: numberArg,
-    });
+  const person = new Person({
+    name: nameArg,
+    number: numberArg
+  })
 
-    person.save().then(result => {
-        console.log(`added ${nameArg} number ${numberArg} to phonebook`);
-        mongoose.connection.close();
-    });
+  person.save().then(result => {
+    console.log(`added ${nameArg} number ${numberArg} to phonebook`)
+    mongoose.connection.close()
+  })
 } else {
-    // Incorrect number of arguments
-    console.log('Usage: node mongo.js <password> [name] [number]');
-    mongoose.connection.close();
-    process.exit(1);
+  // Incorrect number of arguments
+  console.log('Usage: node mongo.js <password> [name] [number]')
+  mongoose.connection.close()
+  process.exit(1)
 }
