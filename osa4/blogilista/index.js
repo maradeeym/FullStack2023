dc41f72f4e2d5c55742f4b39bd60b7d16a2bc86f
Martin <martin.kalliola@gmail.com>
2024-01-05 15:00:44 +0200

    x

--- osa4/blogilista/index.js
+++ osa4/blogilista/index.js
@@ -2,44 +2,15 @@ const http = require('http')
 const express = require('express')
 const app = express()
 const cors = require('cors')
-const mongoose = require('mongoose')
-
-const blogSchema = mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
-})
-
-const Blog = mongoose.model('Blog', blogSchema)
-
-const mongoUrl = `mongodb+srv://serkkupoika:serkkupoika@cluster0.sgksjwl.mongodb.net/blogilista?retryWrites=true&w=majority`
-mongoose.connect(mongoUrl)
+const blogsRouter = require('./controllers/blogs')
+require('./mongo') // Tämä luo yhteyden MongoDB-tietokantaan
 
 app.use(cors())
 app.use(express.json())
-
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
+app.use('/api/blogs', blogsRouter)
 
 const PORT = 3003
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
 
-//npm testi
