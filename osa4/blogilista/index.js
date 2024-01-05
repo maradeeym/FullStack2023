0295f2a519cf0b61b1abd12dc5e1f3018ad38826
Martin <martin.kalliola@gmail.com>
2024-01-06 00:33:01 +0200

    x

--- osa4/blogilista/index.js
+++ osa4/blogilista/index.js
@@ -3,11 +3,13 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const blogsRouter = require('./controllers/blogs')
+const usersRouter = require('./controllers/users')
 require('./mongo') // Tämä luo yhteyden MongoDB-tietokantaan
 
 app.use(cors())
 app.use(express.json())
 app.use('/api/blogs', blogsRouter)
+app.use('/api/users', usersRouter)
 
 const PORT = 3003
 app.listen(PORT, () => {
