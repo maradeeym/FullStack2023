deae3e9954ca95685daed7c44d0feadc2a90a6ec
Martin <martin.kalliola@gmail.com>
2024-01-07 01:27:00 +0200

    4.19

--- osa4/blogilista/controllers/blogs.js
+++ osa4/blogilista/controllers/blogs.js
@@ -1,22 +1,50 @@
-const blogsRouter = require('express').Router()
-const Blog = require('../models/blog')
+const blogsRouter = require('express').Router();
+const Blog = require('../models/blog');
+const User = require('../models/user'); // Import the User model
+const jwt = require('jsonwebtoken');
 
 blogsRouter.get('/', (request, response) => {
   Blog
     .find({})
     .then(blogs => {
-      response.json(blogs)
-    })
-})
+      response.json(blogs);
+    });
+});
 
-blogsRouter.post('/', (request, response) => {
-  const blog = new Blog(request.body)
+const getTokenFrom = request => {
+    const authorization = request.get('authorization');
+    if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+      return authorization.substring(7);
+    }
+    return null;
+}
 
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
+blogsRouter.post('/', async (request, response) => {
+  try {
+    const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET);
+    if (!decodedToken.id) {
+      return response.status(401).json({ error: 'perese' });
+    }
 
-module.exports = blogsRouter
+    const user = await User.findById(decodedToken.id);
+
+    if (!user) {
+      return response.status(401).json({ error: 'user not found' });
+    }
+
+    const blog = new Blog({
+      ...request.body,
+      user: user._id // Associate blog with user
+    });
+
+    const savedBlog = await blog.save();
+    user.blogs = user.blogs.concat(savedBlog._id); // Add blog to user's blogs
+    await user.save();
+
+    response.status(201).json(savedBlog);
+  } catch (error) {
+    response.status(400).json({ error: error.message });
+  }
+});
+
+module.exports = blogsRouter;
--- osa4/blogilista/index.js
+++ osa4/blogilista/index.js
@@ -4,12 +4,14 @@ const app = express()
 const cors = require('cors')
 const blogsRouter = require('./controllers/blogs')
 const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
 require('./mongo') // Tämä luo yhteyden MongoDB-tietokantaan
 
 app.use(cors())
 app.use(express.json())
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
+app.use('/api/login', loginRouter)
 
 const PORT = 3003
 app.listen(PORT, () => {
